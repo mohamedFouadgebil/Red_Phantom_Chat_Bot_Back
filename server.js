0b4976276a54454e49bfa3da1811c7405298f6fe
mohamedFouadgebil <mohamed.foouad987@gmail.com>
2026-05-06 22:32:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const toolsRoutes = require("./routes/tools");
 const chatRoutes = require("./routes/chat");
 
 const app = express();
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
@@ -28,18 +29,27 @@ app.use("/api/tools", toolsRoutes);
 app.use("/api/chat", chatRoutes);
 
 app.get("/", (req, res) => {
-  console.log("Done");
+  res.status(200).json({ status: "success", project: "Red Phantom" });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/index.html"), (err) => {
+    if (err) {
+      res.status(404).send("Frontend not found");
+    }
+  });
 });
 
-const PORT = process.env.PORT || 3000;
 mongoose
-  .connect(process.env.MONGO_URI, {})
-  .then(() => {
-    console.log("Mongo connected");
-    app.listen(PORT, () => console.log("Server running on", PORT));
-  })
-  .catch((err) => console.error(err));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongo connected"))
+  .catch((err) => console.error(err.message));
+
+module.exports = app;
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
